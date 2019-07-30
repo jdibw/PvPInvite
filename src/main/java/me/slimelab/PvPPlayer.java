c256da8749ae1603b43a4d598e6cb76c1041ce76
jdibw <52377770+jdibw@users.noreply.github.com>
2019-07-30 16:24:16 +0800

    20190730 16:24

--- src/main/java/me/slimelab/PvPPlayer.java
+++ src/main/java/me/slimelab/PvPPlayer.java
@@ -9,6 +9,7 @@ public class PvPPlayer {
     UUID uuid;
     public boolean pvping = false;
     public ArrayList<UUID> opponents = new ArrayList<>();
+    public ArrayList<UUID> invites = new ArrayList<>();
 
     public PvPPlayer(UUID uuid){
         this.uuid = uuid;
@@ -20,6 +21,14 @@ public class PvPPlayer {
 
     public void removeOpponent(UUID uuid){
         this.opponents.remove(uuid);
+    }
+
+    public void addInvites(UUID uuid){
+        this.invites.add(uuid);
+    }
+
+    public void removeInvites(UUID uuid){
+        this.invites.remove(uuid);
     }
 
     public void setUUID(UUID uuid){
